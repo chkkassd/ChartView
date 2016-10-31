c5e82d56d786c6f44a15c9a851f6d984cf331343
peter.shi <532289386@qq.com>
2016-10-31 15:31:01 +0800

    Initial Commit

---
+++ ChartView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChartView
+//
+//  Created by 赛峰 施 on 2016/10/31.
+//  Copyright © 2016年 赛峰 施. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
